0d8422f64fe683ccad51eaea869100cdcfe8ee05
Peter Suggitt <me@suggs.org.uk>
2016-07-04 07:25:32 +0100

    added skeleton dir structure for python projects

---
+++ skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except: ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Peter Suggitt',
+    'url': '',
+    'download_url': '',
+    'author_email': 'me@suggs.org.uk',
+    'version':, '0.0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name', 'projectname'
+}
+
+setup(**config)
